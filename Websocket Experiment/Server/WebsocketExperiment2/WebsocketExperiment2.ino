bb1556edba53b2956a14e184525098ea9bd52852
toastedcode <jasontost@gmail.com>
2016-10-03 20:02:51 -0400

    IpServer class fixes.

--- Websocket Experiment/Server/WebsocketExperiment2/WebsocketExperiment2.ino
+++ Websocket Experiment/Server/WebsocketExperiment2/WebsocketExperiment2.ino
@@ -16,12 +16,25 @@ public:
 
    virtual void handleMessage(MessagePtr message)
    {
-      // Format: {"variable1":"", "variable2":""}
-      Logger::logDebug("Received message: " + 
-                       message->getMessageId() + 
-                       " (variable1 = " + 
-                       message->getString("variable1") + 
-                       ")");
+      Logger::logDebug("Received message: " + message->getMessageId() + "\n");
+
+      if (message->getMessageId() == "ping")
+      {
+         Logger::logDebug("Received: ping\n");
+        
+         MessagePtr reply = Messaging::newMessage();
+         if (reply)
+         {
+            reply->setMessageId("pong");
+            reply->setDestination(message->getSource());
+            reply->setSource(getAddress());
+            Messaging::send(reply); 
+         }
+      }
+      else
+      {
+        Logger::logDebug("Unhandled message: " + message->getMessageId() + "\n");
+      }
    }
 };
 
@@ -33,20 +46,20 @@ void setup()
    Logger::setEnabled(true);
    
    // Connect to a network via the ESP8266 wifi adapter.
-   //if (Esp8266::getInstance()->connectWifi("NETGEAR69", "silentsky723", 15) == false)
-   if (Esp8266::getInstance()->connectWifi("compunetix-guest", "compunetix", 15) == false)
+   if (Esp8266::getInstance()->connectWifi("NETGEAR69", "silentsky723", 15) == false)
+   //if (Esp8266::getInstance()->connectWifi("compunetix-guest", "compunetix", 15) == false)
    {
       // If the ESP8266 fails to connect with the stored credentials, we'll create an AP to allow for wifi config.
       Esp8266::getInstance()->startAccessPoint("TOASTBOT", "");
    } 
-  
-   ToastBot::setup("myMachine");
 
    ToastBot::add(new MyComponent("component1"));
-
-   ToastBot::add(new WebSocketAdapter("adapter1", new JsonProtocol()));
+   
+   //ToastBot::add(new WebSocketAdapter("adapter1", new JsonProtocol()));
 
    ToastBot::add(new IpServerAdapter("adapter2", new JsonProtocol(), 80));
+
+   ToastBot::setup("myMachine");
 }
 
 void loop()
