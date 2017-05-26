d99285c7dc6314f2d39bc1334605765071a16295
toastedcode <jasontost@gmail.com>
2017-05-25 23:19:20 -0400

    Configure wifi from properties file.

--- Robox/Robox.ino
+++ Robox/Robox.ino
@@ -12,115 +12,7 @@
 #include <ToastBot.h>
 #include <WebSocketServer.h>
 
-#include "Robox.hpp"
-
-/*
-class Robox : public Component
-{
-  
-public:
-  
-   // Constructor.
-   inline Robox() :
-      Component("robox") {}
-
-   // Destructor.
-   inline virtual ~Robox() {}
-
-   // This operation handles a message directed to this sensor.
-   virtual void handleMessage(
-      // The message to handle.
-      MessagePtr message);
-};
-
-void Robox::handleMessage(
-      MessagePtr message)
-{
-   //  ping
-   if (message->getMessageId() == "ping")
-   {
-      Message* reply = Messaging::newMessage();
-      
-      reply->setMessageId("pong");
-      reply->setSource(getId());
-      reply->setDestination(message->getSource());
-      reply->set("deviceId", ToastBot::getId());
-
-      if (WifiBoard::getBoard())
-      {
-        unsigned char mac[6] = {0, 0, 0, 0, 0, 0};
-        WifiBoard::getBoard()->getMacAddress(mac);
-        char macAddress[18];
-        sprintf(macAddress, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-        
-        reply->set("macAddress", macAddress);
-        reply->set("ipAddress", WifiBoard::getBoard()->getIpAddress());
-      }
-      
-      Messaging::send(reply);
-
-      message->setFree();   
-   }
-   else if (message->getMessageId() == "reset")
-   {
-      Logger::logDebug("Resetting ...");
-      Board::getBoard()->reset();
-   }
-   else if (message->getMessageId() == "property")
-   {
-      String propertyName = message->getString("name");
-      String propertyValue = message->getString("value");
-
-      if (propertyName != "")
-      {
-         Properties properties("/toastbot.properties");
-
-         if (propertyValue != "")
-         {
-            properties.set(propertyName, propertyValue);
-            Logger::logDebug("Updated properties: %s = %s", propertyName.c_str(), propertyValue.c_str());
-         }
-         else
-         {
-            properties.remove(propertyName);
-            Logger::logDebug("Removed property: %s", propertyName.c_str());
-         }
-
-         properties.save();
-      }
-   }
-   else if (message->getMessageId() == "digitalWrite")
-   {
-      int pin = message->getInt("pin");
-      int value = message->getInt("value");
-
-      Logger::logDebug("digitalWrite(%d, %d)", pin, value);
-      Board::getBoard()->digitalWrite(pin, value);
-   }
-   else if (message->getMessageId() == "analogWrite")
-   {
-      int pin = message->getInt("pin");
-      int value = message->getInt("value");
-
-      Logger::logDebug("analogWrite(%d, %d)", pin, value);
-      Board::getBoard()->analogWrite(pin, value);
-   }
-   else if (message->getMessageId() == "bridge")
-   {
-      String name = message->getString("name");
-      String host = message->getString("host");
-      int port = message->getInt("port");
-      
-      Logger::logDebug("bridge(%s, %s:%d)", name.c_str(), host.c_str(), port);
-      
-      ToastBot::add(new TcpClientAdapter(name, new JsonProtocol(), host, port));
-   }
-   else
-   {
-      Component::handleMessage(message);
-   }
-}
-*/
+#include "Robox.h"
 
 void setup()
 {
@@ -137,10 +29,11 @@ void setup()
    WifiBoard* board = new Esp8266Board();
    Board::setBoard(board);
 
-   // Connect to a known network.
-   //if (board->connectWifi("NETGEAR69", "silentsky723", 15) == false)
-   //if (board->connectWifi("Massive", "getshitdone", 15) == false)
-   if (board->connectWifi("compunetix-guest", "compunetix", 15) == false)
+   // Connect to a wifi network.
+   String ssid = properties.getString("wifi.ssid");
+   String password = properties.getString("wifi.password");
+   if ((ssid == "") ||
+       (board->connectWifi(ssid, password, 15) == false))
    {
       // If the ESP8266 fails to connect with the stored credentials, we'll create an AP to allow for wifi config.
       board->startAccessPoint("TOASTBOT", "");
@@ -148,7 +41,7 @@ void setup()
 
    Logger::logDebug("Starting TOASTBOT");
 
-   ToastBot::add(new Robox(), true);// <-- default handler
+   ToastBot::add(new Robox(), true);  // <-- default handler
 
    Motor* motor1 = new Motor("motor1", 0, 5);
    Motor* motor2 = new Motor("motor2", 2, 4);
@@ -190,8 +83,6 @@ void setup()
 
    ToastBot::setup(properties.getString("deviceName"));
 }
-
-long retryTime = millis() + 5000;;
 
 void loop()
 {
