042c14b25e31dbd9e47404fa2a6f77984434f565
toastedcode <jasontost@gmail.com>
2017-12-21 06:56:08 -0500

    HttpClientAdapter

---
+++ src/Adapter/HttpClientAdapter.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <ESP8266HTTPClient.h>
+
+#include "Adapter.hpp"
+#include "../Messaging/RestfulProtocol.hpp"
+
+class HttpClientAdapter : public Adapter
+{
+
+public:
+
+	HttpClientAdapter(
+      const String& id,
+      Protocol* protocol);
+
+   virtual bool sendRemoteMessage(
+      MessagePtr message);
+
+   virtual MessagePtr getRemoteMessage();
+
+private:
+
+   HTTPClient http;
+
+   RestfulProtocol sendProtocol;
+};
